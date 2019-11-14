b836ad21f780203b9c7db608247b29f501c9fe20
lilyei <53099752+lilyei@users.noreply.github.com>
2019-11-14 12:37:28 -0600

    update
    Co-Authored-By: sistemas-ae <sistemas-ae@users.noreply.github.com>

--- generar.php
+++ generar.php
@@ -1,18 +1,31 @@
 <?php
 require 'fpdf/fpdf.php';
 
+
+
 $empresa=$_POST['empresa'];
 $trabajo=$_POST['trabajo'];
 $cargo=$_POST['cargo'];
 $depto =$_POST['depto'];
 $ing=$_POST['ing'];
 $actividad=$_POST['actividad'];
+
+//echo $empresa;
+//echo $trabajo;
+//echo $cargo;
+//echo $depto;
+//echo $ing;
+//echo $actividad;
+
+
  $pdf=new FPDF();
  $pdf->AddPage();
  $logo="logosmart.png";
 $Fecha=  date("d-m-Y ");
 
-
+/**
+ * }
+ */
 class pdf extends FPDF
 {
     
---
+++ login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="lil.css">
+  <meta charset="utf-8">
+  <meta charset="utf-16">
+  <title>Register</title>
+  <link rel="stylesheet"  href="login.css">
+
+  <div align="center" class="user-login-box">
+    <span class="user-icon"></span>
+    <form method="POST" action="user.php" >
+      <h1 class="lil">lil</h1>
+    <div class="user-name" style="font-size: 40px;">LOGIN</div>   <br>
+    <label class="la">Usuario</label>
+    <input id="username" name="username" class="user-password" align="center" type="text" />
+   <h1 class="lil">lil</h1>
+   <label class="la">Contraseña</label>
+    <input id="password" name="password" class="user-password" align="center" type="password" />
+  <h1 class="lil">lil</h1>
+  <input type="submit" class="log" value="LOGIN">
+ <h1 class="lil">lil</h1>
+    </form>
+  </div>
+<body>
+</body>
+</html>
